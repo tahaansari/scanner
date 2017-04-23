3cf85e7fe6d2df831075995c6c592f9401e705a6
= <taha.m.ansari@gmail.com>
2017-04-23 14:30:08 +0530

    added tick

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,13 +1,13 @@
 // Initialize app
 var myApp = new Framework7({
-
     modalTitle: 'Scanner',
     onAjaxStart: function (xhr) {
         myApp.showIndicator();
     },
     onAjaxComplete: function (xhr) {
         myApp.hideIndicator();
-    }
+    },
+    preloadPreviousPage:true,
 });
 
 // If we need to use custom DOM library, let's save it to $$ variable:
@@ -49,13 +49,54 @@ $$(document).on('deviceready', function() {
         }
 
     }, false);
+
+    
 });
 
 
 myApp.onPageInit('index', function (page) {
 
-      console.log('index page called');
+    console.log('index page called');
+
+    $('.btn-scan').click(function(){
+        cordova.plugins.barcodeScanner.scan(
+          function (result) {
+
+            if(!result.cancelled){
+                mainView.router.load({
+                    url:"result.html",
+                    query:{
+                        resultText: result.text,
+                        resultFormat: result.format
+                    }
+                })
+                
+            }else{
+
+                mainView.router.load({
+                    url:"index.html"
+                })
+            }
+          },
+          function (error) {
+              alert("Scanning failed: " + error);
+          },
+          {
+              preferFrontCamera : false, // iOS and Android
+              showFlipCameraButton : true, // iOS and Android
+              showTorchButton : true, // iOS and Android
+              torchOn: false, // Android, launch with the torch switched on (if available)
+              prompt : "Place a barcode inside the scan area", // Android
+              resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+              formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+              orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+              disableAnimations : true, // iOS
+              disableSuccessBeep: false // iOS
+          }
+        );
+    })
 })
+
 
 myApp.onPageInit('result', function (page) {
 
